f7b5cd0aa22010c19c06a5b2e777ed782e323ef7
Dias <orazbaevdev@gmail.com>
2022-06-08 10:48:11 +0600

    Init repository

---
+++ src/main/java/kz/fastjava/javaops/JavaOpsApplication.java
@@ -0,0 +1,13 @@
+package kz.fastjava.javaops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOpsApplication.class, args);
+    }
+
+}
